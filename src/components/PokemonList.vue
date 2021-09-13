aaf1192a375f0255d5609ef845f6d2490dd89874
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-13 13:29:14 -0300

    Componente de Lista

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,13 +1,62 @@
 <template>
-    <h1>Pokemon List</h1>
+  <h1>Pokemon App</h1>
+  <loader v-if="isLoading"/>
+  <div 
+  class="div-pokemon" 
+  v-for="(pokemon, index) in list" 
+  :key="index" 
+  @click="selectPokemon(getPokemonId(pokemon.url))" > 
+      <img :src="getPokemonImage(getPokemonId(pokemon.url))" >
+      {{ pokemon.name }}
+    </div>
 </template>
 
 <script>
+import { getPokemonList, getPokemonImageUrl } from "../service/pokemon-service.js";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import Loader from "./Loader.vue";
+
+
 export default {
-    
-}
+    components:{
+        Loader,
+    },
+  setup() {
+      //executada antes da criação do componente
+    const list = ref([]);
+    const router = useRouter();
+    const isLoading = ref(false);
+
+    onMounted(() => {
+        isLoading.value = true;
+      getPokemonList().then((resp) => {
+          console.log(resp);
+          list.value = resp.results;
+      }).finally(() => {
+          isLoading.value = false;
+      });
+    });
+
+    const getPokemonId = (url) =>
+        url.replace("https://pokeapi.co/api/v2/pokemon/", "").replace("/", "");
+
+    const getPokemonImage = (id) => getPokemonImageUrl(id);
+
+    const selectPokemon = (id) => router.push({ name: "pokemon", params: { id }});
+
+    return { list, getPokemonImage, isLoading, getPokemonId, selectPokemon };
+  },
+};
 </script>
 
 <style scoped>
-
+.div-pokemon{
+    display: flex;
+    align-items: center;
+    border: 1px solid lightgray;
+    border-radius: 0.2rem;
+    margin: 0.5rem;
+    cursor: pointer;
+}
 </style>

645e8757c70e88ccb9744b9e6f0b31ec39de363e
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-13 13:29:28 -0300

    Importação das rotas da Aplicação

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
+
+//Importando a rota
+import { createRouter, createWebHistory } from "vue-router";
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Importando os componentes para criação das páginas
+import PokemonList from "./components/PokemonList.vue";
+import PokemonDetail from "./components/PokemonDetail.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: PokemonList },
+        { path: "/pokemon/id", name: "pokemon", component: PokemonDetail }
+    ]
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
